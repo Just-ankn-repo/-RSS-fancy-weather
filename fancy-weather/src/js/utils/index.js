9f27c357145190204f9198eb067f57d32bfa036a
Andrey Knyshenko <andrei.knyshenko@gmail.com>
2020-06-01 01:27:00 +0400

    refactor: delete unused util

--- fancy-weather/src/js/utils/index.js
+++ fancy-weather/src/js/utils/index.js
@@ -1,11 +1,9 @@
 import combineWeatherData from './combineWeatherData';
 import combineData from './combineData';
 import setListener from './setListener';
-import createBackgroundImage from './createBackgroundImage';
 
 export default {
   combineWeatherData,
   combineData,
   setListener,
-  createBackgroundImage,
 };
