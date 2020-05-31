bc32f11a78ba5bece2037f5d76194abb6091ef70
Andrey Knyshenko <andrei.knyshenko@gmail.com>
2020-05-31 17:27:11 +0400

    feat: add config file

---
+++ fancy-weather/src/config/env.config.js
@@ -0,0 +1,11 @@
+export default {
+  unsplashUrl: 'https://api.unsplash.com',
+  unsplashToken: 'JHnipoEEO0LP4jLnOTH3yPu8Wm7JvEPhkfZ0LXhea38',
+  openweathermapUrl: 'https://api.openweathermap.org',
+  openweathermapToken: 'fdb0b6b915067fbc9039abcf71a00e18',
+  mapboxToken: 'pk.eyJ1IjoianVzdC1hbmtuIiwiYSI6ImNrYXRnbXN0MjA1eWsycnBnYWxkamwwcGQifQ.13aDpx4cow2eOHJNSv-AFg',
+  opencageUrl: 'https://api.opencagedata.com',
+  opencageToken: '18d112357b7a47869537fad597523524',
+  ipstackUrl: 'http://api.ipstack.com',
+  ipstackToken: 'af523bfb0479f9d049d74b40d4b23bda',
+};
