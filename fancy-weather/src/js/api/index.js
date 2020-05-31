d667f9e88eb6e531335c79d09f00920c973bdb57
Andrey Knyshenko <andrei.knyshenko@gmail.com>
2020-05-31 21:35:25 +0400

    refactor: move some files

--- fancy-weather/src/js/api/index.js
+++ fancy-weather/src/js/api/index.js
@@ -2,12 +2,10 @@ import Unsplash from './unsplash.api';
 import Weather from './openweathermap.api';
 import GeocodingByCity from './opencagedata.api';
 import CityByIP from './ipstack.api';
-import Map from './mapbox';
 
 export default {
   Unsplash,
   Weather,
   GeocodingByCity,
   CityByIP,
-  Map,
 };
--- fancy-weather/src/js/index.js
+++ fancy-weather/src/js/index.js
@@ -1,4 +1,4 @@
-import Controller from './modules/index';
+import Controller from './components/controller';
 
 const controller = new Controller();
 controller.init();
