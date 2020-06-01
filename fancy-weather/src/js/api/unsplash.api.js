73aa1395935afccced8c13af075e75ba83c49f45
Andrey Knyshenko <andrei.knyshenko@gmail.com>
2020-06-02 02:28:59 +0400

    fix: add error catch to image api

--- fancy-weather/src/js/api/unsplash.api.js
+++ fancy-weather/src/js/api/unsplash.api.js
@@ -10,18 +10,23 @@ export default class Unsplash {
 
   async searchImage(query) {
     this.lastQuery = query || this.lastQuery;
-    console.log(this.lastQuery);
-    const response = await fetch(`${this.apiUrl}/photos/random?orientation=landscape&
-      per_page=1&query={${this.lastQuery}}&client_id=${this.apiToken}`);
-    const result = await response.json();
+    let result = {};
+    try {
+      console.log(this.lastQuery);
+      const response = await fetch(`${this.apiUrl}/photos/random?orientation=landscape&
+        per_page=1&query={${this.lastQuery}}&client_id=${this.apiToken}`);
+      result = await response.json();
+    } catch (e) {
+      result.urls = { full: './assets/img/default-background.jpg' };
+    }
 
     return new Promise((resolve) => {
       const imgElement = new Image();
       imgElement.src = result.urls.full;
-      imgElement.classList.add('background-image');
+      imgElement.classList = 'background-image';
       imgElement.onload = () => resolve(imgElement);
       imgElement.onerror = () => {
-        imgElement.src = './assets/img/default-background.png';
+        imgElement.src = './assets/img/default-background.jpg';
         imgElement.onload = () => resolve(imgElement);
       };
     });
